708cd80f5d6f166a02a6fe6b7d1f0aeadd3f08be
erixp <ericklopezdiaz70@gmail.com>
2025-05-09 13:03:13 -0600

    Delete cerses.php

--- cerses.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Eliminar todas las variables de sesión
-session_destroy(); // Destruir la sesión
-header("Location: inicio.php"); // Redirigir a la página de inicio
-exit();
-?>
